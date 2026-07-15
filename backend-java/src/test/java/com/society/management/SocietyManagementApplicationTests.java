f1b16557bb8fb81e8c9580e07bbad7172896b351
anupbobade14-jpg <anup.bobade14@gmail.com>
2026-07-15 11:57:27 +0530

    Add files via upload

---
+++ backend-java/src/test/java/com/society/management/SocietyManagementApplicationTests.java
@@ -0,0 +1,22 @@
+package com.society.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "spring.flyway.enabled=false",
+        "app.mail.enabled=false"
+})
+class SocietyManagementApplicationTests {
+
+    @Test
+    void contextLoads() { }
+}
